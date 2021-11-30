6ed9af9102cebdf50cc23177f1f9e0d883b0a654
reimei <34594195+Reimei1213@users.noreply.github.com>
2021-11-30 13:53:15 +0900

    fix

--- main.py
+++ main.py
@@ -1,36 +1,9 @@
-import base64
-from Crypto import Random
-from Crypto.Cipher import AES
+from simple_aes_cipher import AESCipher, generate_secret_key
 
-KEY = "nEc1IiX43XaCaIL51JXyFyyYzeYmDSlArSh82eq3UDpqYrEHkc"
+pass_phrase = "hogefuga"
+secret_key = generate_secret_key(pass_phrase)
 
-class AESCipher(object):
-    def __init__(self, key, block_size=32):
-        self.bs = block_size
-        if len(key) >= len(str(block_size)):
-            self.key = key[:block_size]
-        else:
-            self.key = self._pad(key)
-    
-    def encrypt(self, raw):
-        raw = self._pad(raw)
-        iv = Random.new().read(AES.block_size)
-        cipher = AES.new(self.key, AES.MODE_CBC, iv)
-        return base64.b64encode(iv + cipher.encrypt(raw))
-    
-    def decrypt(self, enc):
-        enc = base64.b64decode(enc)
-        iv = enc[:AES.block_size]
-        cipher = AES.new(self.key, AES.MODE_CBC, iv)
-        return self._unpad(cipher.decrypt(enc[AES.block_size:]))
-
-    def _pad(self, s):
-        return s + (self.bs - len(s) % self.bs) * chr(self.bs - len(s) % self.bs)
-    
-    def _unpad(self, s):
-        return s[:-ord(s[len(s)-1:])]
-        
-cipher = AESCipher(KEY)
+cipher = AESCipher(secret_key)
 with open('secret.txt') as f:
     data = f.read()
-print(cipher.decrypt(data).decode())
+print(cipher.decrypt(data))
